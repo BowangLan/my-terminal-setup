f6ed13e8e09da7bd930c57cb479468ab160167c3
BowangLan <60339191+BowangLan@users.noreply.github.com>
2022-09-10 18:43:40 -0700

    Create zsh-setup.sh

---
+++ zsh-setup.sh
@@ -0,0 +1,4 @@
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
